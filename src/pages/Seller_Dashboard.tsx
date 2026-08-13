acd5d664782f0c71581759c3f6b4fe1718ab59e2
sureshkumar-dev <sureshpackirisamy05@gmail.com>
2026-08-13 21:41:15 +0530

    seller stats price updated 2

--- src/pages/Seller_Dashboard.tsx
+++ src/pages/Seller_Dashboard.tsx
@@ -193,6 +193,7 @@ const Seller_Dashboard = () => {
         console.log(res);
         fetchproducts();
     }
+    const revenue = order.reduce((acc,curr)=>(curr.price + acc),0)
     return (
         <>
             <nav className='w-full h-max  flex mx-auto justify-center shadow-[0_2px_12px_rgba(0,0,0,0.06)]  p-1 '>
@@ -220,7 +221,7 @@ const Seller_Dashboard = () => {
                         <div className='bg-white rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] h-[140px]'>
                             <div className='w-full h-full p-4 flex flex-col gap-3 justify-center'>
                                 <img className='h-[30px] w-[30px]' src={money} alt="money" />
-                                <h1 className='md:text-xl lg:text-3xl font-[500]'>₹{order.reduce((acc,curr)=>(curr.price + acc),0)} </h1>
+                                <h1 className='md:text-xl lg:text-3xl font-[500]'>₹{revenue} </h1>
                                 <p className='md:text-md lg:text-2xl text-gray-600 font-[400]'>Total Revenue</p>
                             </div>
                         </div>
