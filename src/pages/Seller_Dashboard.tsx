dda4bdff5e44f6dd69fb0994ae4770e040dbf39b
sureshkumar-dev <sureshpackirisamy05@gmail.com>
2026-08-13 22:12:40 +0530

    seller stats price updated 8

--- src/pages/Seller_Dashboard.tsx
+++ src/pages/Seller_Dashboard.tsx
@@ -257,7 +257,7 @@ const Seller_Dashboard = () => {
                         <div className='bg-white rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] h-[140px]'>
                             <div className='w-full h-full p-4 flex flex-col gap-3 justify-center'>
                                 <img className='h-[30px] w-[30px]' src={pending} alt="money" />
-                                <h1 className='md:text-xl lg:text-3xl font-[500]'>12</h1>
+                                <h1 className='md:text-xl lg:text-3xl font-[500]'>{pendingOrders.length}</h1>
                                 <p className='md:text-md  lg:text-2xl text-gray-600 font-[400]'>Pending Orders</p>
                             </div>
                         </div>
